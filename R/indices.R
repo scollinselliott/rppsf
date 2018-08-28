0e446d2051862f46cb6bf82539514ab672062199
Stephen A. Collins-Elliott <sce@utk.edu>
2018-08-28 15:47:19 -0400

    Update indices.R

--- R/indices.R
+++ R/indices.R
@@ -7,19 +7,6 @@ library(fitdistrplus)
 library(devtools)
 library(cowplot)
 
-setwd("E:/Research/2018 Stats/Generalized MC Resampling/") 
-
-CNcoins = read.csv("gammavalues-['CN'].csv")
-PTcoins = read.csv("gammavalues-['PT'].csv")
-PIcoins = read.csv("gammavalues-['PI'].csv")
-MZcoins = read.csv("gammavalues-['MZ'].csv")
-daterange <- MZcoins[,1]
-
-CN.sub = read.csv("rmeancollected-subsampled-['CN'].csv")
-PT.sub = read.csv("rmeancollected-subsampled-['PT'].csv")
-PI.sub = read.csv("rmeancollected-subsampled-['PI'].csv")
-MZ.sub = read.csv("rmeancollected-subsampled-['MZ'].csv")
-
 setwd("E:/Research/2018 Small Finds/2017 JMA Revision/scripts") 
 
 ######################
