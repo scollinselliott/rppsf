5dc4e24230f82a0a0d72e23b4e52ff990b80d891
Stephen A. Collins-Elliott <sce@utk.edu>
2018-11-07 22:26:25 -0500

    Update mds.r

--- R/mds.r
+++ R/mds.r
@@ -1,10 +1,10 @@
+library(MASS)
 library(coda)
 library(ggplot2)
 library(reshape2)
 library(fitdistrplus)
 library(MCMCpack)
 library(fitdistrplus)
-library(devtools)
 library(cowplot)
 
 setwd("C:/...")  #insert working directory
@@ -79,20 +79,6 @@ pa$MON	<-	NULL
 pt$MON	<-	NULL
 pi1$MON	<-	NULL
 pi2$MON	<-	NULL
-
-#mz1$exchange	<-	NULL
-#mz2$exchange	<-	NULL
-#mz3$exchange	<-	NULL
-#tb1$exchange	<-	NULL
-#tb2$exchange	<-	NULL
-#tb3$exchange	<-	NULL
-#cn1$exchange	<-	NULL
-#cn2$exchange	<-	NULL
-#sm$exchange	<-	NULL
-#pa$exchange	<-	NULL
-#pt$exchange	<-	NULL
-#pi1$exchange	<-	NULL
-#pi2$exchange	<-	NULL
 
 mz1label <- rep("MZ1",1000)
 mz2label <- rep("MZ2",1000)
@@ -170,6 +156,7 @@ total <- rbind(mz1,mz2,mz3,pi1,pi2,cn1,cn2,sm,pa,pt,tb1,tb2,tb3)
 ###########################################################################################
 
 #resample from the kde: n = number of samples, z = density of original data, x = original data 
+#https://stats.stackexchange.com/questions/321542/how-can-i-draw-a-value-randomly-from-a-kernel-density-estimate
 rdens <- function(n, density=z, data=x, kernel="gaussian") {
   width <- z$bw                              # Kernel width
   rkernel <- function(n) rnorm(n, sd=width)  # Kernel sampler
@@ -240,14 +227,10 @@ colnames(means2) <- domain2
 colnames(means3) <- domain3
 
 
-library(MASS)
-
 total2 <- dcast(total, phase ~ variable)
 rownames(total2) <- total2$phase
 total2$phase <- NULL
 total2[] <- NA
-
-
 
 for (i in rownames(total2)) {
 for (j in colnames(total2)) {
@@ -345,113 +328,3 @@ title <- ggdraw() +
 p <- plot_grid(p.mds, p.mds.economic, p.mds.cprod, p.mds.exchange, ncol=2)
 plot_grid(title, p, ncol = 1, rel_heights = c(0.1, 1.5)) # rel_heights values control title margins
 
-
-#################################
-#################################
-#
-# to calculate distance measure between late republican and late antique phases
-#
-#################################
-#################################
-#
-# take the distances between each non null value
-# and then average them
-#
-#
-
-rjcdist <- c()
-
-for (b in 1:1000) {
-	total2 <- dcast(total, phase ~ variable)
-	rownames(total2) <- total2$phase
-	total2$phase <- NULL
-	total2[] <- NA
-
-	total2 <- total2[ which(rownames(total2) %in% c("PI1", "CN1", "MZ1", "MZ2", "PA", "PT", "SM", "TB1", "TB2")), ]
-
-	for (i in rownames(total2)) {
-	for (j in colnames(total2)) {
-	t <- total[total$phase == i & total$variable == j, ]
-	x <- mean(t$value) #to check and see if not NA
-	if ( is.finite(x) ) {
-	#dmax <- which.max(density(t$value)$y)
-	#x <- density(t$value)$x[dmax] # mean(t$value)      # t[sample(nrow(t), 1), ]$value
-	total2[i,j] <- sample(t$value, size = 1)
-	}
-	}
-	}
-
-	d <- mean(dist(total2))
-	rjcdist <- append(rjcdist,d)
-}
-
-ladist <- c()
-
-for (b in 1:1000) {
-	total2 <- dcast(total, phase ~ variable)
-	rownames(total2) <- total2$phase
-	total2$phase <- NULL
-	total2[] <- NA
-
-	total2 <- total2[ which(rownames(total2) %in% c("CN2", "MZ3", "PI2", "TB3")), ]
-
-	for (i in rownames(total2)) {
-	for (j in colnames(total2)) {
-	t <- total[total$phase == i & total$variable == j, ]
-	x <- mean(t$value) #to check and see if not NA
-	if ( is.finite(x) ) {
-	#dmax <- which.max(density(t$value)$y)
-	#x <- density(t$value)$x[dmax] # mean(t$value)      # t[sample(nrow(t), 1), ]$value
-	total2[i,j] <- sample(t$value, size = 1)
-	}
-	}
-	}
-
-	d <- mean(dist(total2))
-	ladist <- append(ladist,d)
-}
-
-x <- seq(0,2,length.out = 1000)
-rjcmean <- mean(rjcdist)
-rjcsd <- sd(rjcdist)
-lamean <- mean(ladist)
-lasd <- sd(ladist)
-rjcdist2 <- rnorm(x, mean = rjcmean, sd = rjcsd)
-
-diff <- data.frame(rjcdist)
-diff$ladist <- ladist
-
-
-p.diff <- ggplot(diff) + geom_density(aes(rjcdist), color = "blue", label = "RJC") +
-geom_density(aes(ladist), color = "red") +
-labs(x="Mean Euclidean Distance between Sites", y = " ") + ggtitle("Site Differentiation Based on Behavioral Indices") + 
-theme_classic()
-p.diff
-
-
-conf = 0.85
-
-hpd_lower = function(x) coda::HPDinterval(as.mcmc(x), prob = conf)[1]
-hpd_upper = function(x) coda::HPDinterval(as.mcmc(x), prob = conf)[2]
-
-library(dplyr)
-
-colnames(diff)[1] <- "Late Rep./Jul.-Claudian"
-colnames(diff)[2] <- "Late Antique"
-
-mdiff <- melt(diff)
-
-df_hpd <- group_by(mdiff, variable) %>% summarize(x=hpd_lower(value), xend=hpd_upper(value))
-
-colnames(diff)[1] <- "Late Rep./Jul.-Claudian"
-colnames(diff)[2] <- "Late Antique"
-
-ggplot(mdiff, aes(x=value, color = variable)) + 
-  geom_density() +
-#  facet_wrap(~variable) + 
-  geom_segment(data = df_hpd, aes(x=x, xend=xend, y=0, yend=0), size=3) +
-#  scale_colour_brewer(direction= -1) +
-scale_color_manual(values = c("#00bfc4","#f8766d")) +
-labs(x="Mean Euclidean Distance between Sites by Period", y = " ") + 
-ggtitle("Site Differentiation Based on Behavioral Indices (85% Cred. Int.)") + 
-theme_classic()
